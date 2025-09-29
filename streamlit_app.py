c26a69b383ca2e1a24104043025c3a0681cf5e4f
pestao233 <pestao@gmail.com>
2025-09-29 22:25:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,13 @@ page=st.sidebar.radio("Aller vers", pages)
 if page == pages[0] : 
   st.write("### Introduction")
 
-st.dataframe(df.head(10))
+  st.dataframe(df.head(10))
 
-st.write("shape:", df.shape)
-st.dataframe(df.describe())
+  st.write("shape:", df.shape)
+  st.dataframe(df.describe())
 
-if st.checkbox("Afficher les NA") :
-  st.dataframe(df.isna().sum())
+  if st.checkbox("Afficher les NA") :
+    st.dataframe(df.isna().sum())
 
 if page == pages[1] : 
   st.write("### DataVizualization")
